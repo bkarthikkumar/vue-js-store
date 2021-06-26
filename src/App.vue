2f92fa1de70b20a5db34e2df68ef85dbbe5c326b
B Karthik Kumar <kumarkarthik.b20@gmail.com>
2021-06-26 17:17:23 +0530

    Added Bootstrap Added product filter , product list  and add to cart page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <Header/>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Products/>
+  <Footersection/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Header from './components/Header.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Footersection from './components/Footer.vue';
+import Products from './components/ProductsList.vue';
 export default {
   name      : 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     Header,
+    Footersection,
+    Products,
   }
 }
 </script>
@@ -24,6 +30,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align             : center;
   color                  : #2c3e50;
-  margin-top             : 60px;
+  margin-top             : 2px;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+ <footer
+    class="text-center">
+    <span class="text-sm">Copyright &COPY; {{year()}} B Karthik Kumar</span>
+  </footer>
+</template>
+<script>
+export default {
+    name   : "Footer File",
+    methods: {
+        year(){
+            return new Date().getFullYear();
+        }
+    }
+}
+</script>
+<style scoped>
+footer {
+    background: #343a40;
+    color     : #fff;
+    /* position: fixed; */
+    bottom : 0;
+    width  : 100%;
+    padding: 15px;
+}
+</style>
